3cbd4cba1b251d42ef8d2b498bae35e2bdd0f5a6
Vadim Laletin <rusmiligamer@gmail.com>
2026-01-21 20:53:41 +0100

    Increase limit for json

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { toFirstClassExtension, fromFirstClassExtension } = require('sdc-qrf/dist/converter/index.js');
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 
 function validateQuestionnaire(req, res, next) {
   if (!req.body || req.body.resourceType !== 'Questionnaire') {
